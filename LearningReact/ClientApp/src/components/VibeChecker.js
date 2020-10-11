158b1448a142163633d38a3262bfc07de14d9acc
Dan <danieljkrajewski@gmail.com>
2020-10-11 13:12:43 -0500

    Display past vibe details now

--- LearningReact/ClientApp/src/components/VibeChecker.js
+++ LearningReact/ClientApp/src/components/VibeChecker.js
@@ -10,7 +10,9 @@ export class VibeChecker extends Component {
         super(props);
         this.state = {
             currentCount: 0,
-            value: ''};
+            value: '',
+            vibeSubmitted: false,
+            allVibes: [{}]};
         this.incrementCounter = this.incrementCounter.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
@@ -25,26 +27,75 @@ export class VibeChecker extends Component {
         });
     }
 
-    static renderOverviewOfVibes() {
+    renderTableData() {
 
+        return this.state.allVibes.map(vibe => {
+            return (
+                <tr key={vibe.id}>
+                    <td>{vibe.id}</td>
+                    <td>{vibe.vibeLevel}</td>
+                    <td>{vibe.vibeDetails}</td>
+                    <td>{vibe.dateRecorded}</td>
+                </tr>
+                )
+        })
     }
 
     render() {
-        return (
-            <div>
-                <row>
-                    <img height="250" width="250" src={HappyFace} alt="HappyVibes" onClick={() => this.addVibe("1")}/>
-                    <img height="250" width="250" src={NeutralFace} alt="NeutralVibes" onClick={() => this.addVibe("0")}/>
-                    <img height="250" width="250" src={SadFace} alt="SadVibes" onClick={() => this.addVibe("-1")}/>
-                    <img height="250" width="250" src={AngryFace} alt="BadVibes" onClick={() => this.addVibe("-2")}/>
-                </row>
-                <div class="form-group">
-                    <label for="vibeDetails">So how was you're day?</label>
-                    <textarea class="form-control" id="vibeDetails" row="2" onChange={this.handleChange} ></textarea>
+        if (!this.state.vibeSubmitted) {
+            return (
+                <div>
+                    <row>
+                        <img height="250" width="250" src={HappyFace} alt="HappyVibes" onClick={() => this.addVibe("1")} />
+                        <img height="250" width="250" src={NeutralFace} alt="NeutralVibes" onClick={() => this.addVibe("0")} />
+                        <img height="250" width="250" src={SadFace} alt="SadVibes" onClick={() => this.addVibe("-1")} />
+                        <img height="250" width="250" src={AngryFace} alt="BadVibes" onClick={() => this.addVibe("-2")} />
+                    </row>
+                    <div class="form-group">
+                        <label for="vibeDetails">So how was you're day?</label>
+                        <textarea class="form-control" id="vibeDetails" row="2" onChange={this.handleChange} ></textarea>
+                    </div>
+
+                </div>
+            );
+        }
+        else {
+            return (
+                <div>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Vibe Level</th>
+                                <th scope="col">Vibe Details</th>
+                                <th scope="col">Date Recorded</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.renderTableData()}
+                        </tbody>
+
+                    </table>
+
                 </div>
 
-            </div>
-        );
+            );
+        }
+        
+    }
+
+    async pullVibeData() {
+        const response = await fetch("vibes", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        var DataTest = await response.json();
+
+        alert(JSON.stringify(DataTest));
+
+        await this.setState({ allVibes: DataTest });
+        this.setState({ vibeSubmitted: true });
     }
 
     async addVibe(vibeLevel) {
@@ -61,6 +112,7 @@ export class VibeChecker extends Component {
                     "VibeDetails": this.state.value
             })
         });
-        
+
+        this.pullVibeData();
     }
 }
